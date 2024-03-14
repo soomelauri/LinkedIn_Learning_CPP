d17deffe15aa0a423cd0aeccc18801d10f4a5be2
soomelauri <143125206+soomelauri@users.noreply.github.com>
2024-03-14 23:36:53 +0000

    Completed Type Conversion C++ exercise

--- src/Ch02/02_10b/CodeDemo.cpp
+++ src/Ch02/02_10b/CodeDemo.cpp
@@ -6,7 +6,18 @@
 #include <cstdint>
 
 int main(){
-    
+    float flt;
+    int32_t sgn;
+    uint32_t unsgn;
+
+    flt = -7.66;
+    sgn = flt;
+    unsgn = sgn;
+
+    std::cout << "The flt is: " << flt << std::endl;
+    std::cout << "The sgn is: " << sgn << std::endl;
+    std::cout << "The unsgn is: " << (int32_t) unsgn << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
