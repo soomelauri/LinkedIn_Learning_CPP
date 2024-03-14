d41a082d822a2c0eb53924022f15ad87a28a8201
soomelauri <143125206+soomelauri@users.noreply.github.com>
2024-03-14 22:57:01 +0000

    Completed the C++ exercise

--- src/Ch02/02_06b/CodeDemo.cpp
+++ src/Ch02/02_06b/CodeDemo.cpp
@@ -3,9 +3,24 @@
 // Preprocessor directives, by Eduardo Corpeño 
 
 #include <iostream>
+#include <string>
+#include <cstdint>
+
+#define DEBUG
+#define CAPACITY 5000
 
 int main(){
-    
+    int32_t large = CAPACITY;
+    uint8_t small = 37;
+
+#ifdef DEBUG
+    std::cout << "[About to perform the addition]" << std::endl;
+#endif
+
+    large += small;
+
+    std::cout << "The value of large is " << large << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
