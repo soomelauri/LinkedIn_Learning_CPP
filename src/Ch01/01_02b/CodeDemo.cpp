2ae2d1894dfc009dd24c45cec52cf23ce90ad0ad
soomelauri <143125206+soomelauri@users.noreply.github.com>
2024-03-14 17:03:30 +0000

    Completed C++ exercise

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,13 @@
 // Learning C++ 
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+# include <iostream> // Include the standard input / output library
+
+int main() { // Start the main program execution
+    std::cout << "Hello World!" << std::endl; // 
+
+    std::cout << std::endl << std::endl;
+
+    return(0);
+}
