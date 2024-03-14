362754aea5fc83cf88431037214b82634121834b
soomelauri <143125206+soomelauri@users.noreply.github.com>
2024-03-14 22:28:33 +0000

    Completed C++ exercise

--- src/Ch02/02_03b/CodeDemo.cpp
+++ src/Ch02/02_03b/CodeDemo.cpp
@@ -2,11 +2,27 @@
 // Exercise 02_03
 // Using Variables, by Eduardo Corpeño 
 
+int a, b = 5;
+
 #include <iostream>
 
 int main(){
-    std::cout << "Hi There!" << std::endl;
+    bool my_flag;
+    a = 7;
+    my_flag = false;
+
+    std::cout << "a is " << a << std::endl;
+    std::cout << "b is " << b << std::endl;
+    std::cout << "my_flag is " << my_flag << std::endl;
+
+    std::cout << "a + b is " << a + b << std::endl;
     
+    std::cout << "b - a is " << b - a << std::endl;
+
+    unsigned int positive;
+    positive = b - a;
+    std::cout << "b - a (unsigned)" << positive << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
