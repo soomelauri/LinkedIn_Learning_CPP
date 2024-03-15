98a7c165b99e9abefcac68f31ccbdab1d6a39a70
soomelauri <143125206+soomelauri@users.noreply.github.com>
2024-03-15 00:16:02 +0000

    C++ exercise

--- src/Ch02/02_11b/CodeDemo.cpp
+++ src/Ch02/02_11b/CodeDemo.cpp
@@ -6,7 +6,19 @@
 #include <cstdint>
 
 int main(){
-    
+    int fahrenheit = 100;
+    int celsius;
+
+    celsius = ((float) 5 / 9.0 ) * (fahrenheit - 32);
+
+    std::cout << "100 degrees Fahrenheit equals " << celsius << " degrees Celsius." << std::endl;
+
+    float weight = 10.99;
+
+    std::cout << "Float             : " << weight << std::endl;
+    std::cout << "Integer part      : " << (int) weight << std::endl;
+    std::cout << "Franctional part  : " << (int)((weight - (int) weight) * 10000) << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
