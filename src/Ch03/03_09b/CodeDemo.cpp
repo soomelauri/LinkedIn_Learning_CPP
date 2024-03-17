783ce5b585db5d1c7bf26da80a3dc2422063b0d0
soomelauri <143125206+soomelauri@users.noreply.github.com>
2024-03-17 01:26:34 +0000

    c++

--- src/Ch03/03_09b/CodeDemo.cpp
+++ src/Ch03/03_09b/CodeDemo.cpp
@@ -4,9 +4,26 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 int main(){
-    
+    std::vector <int> primes;
+
+    primes.push_back(2);
+    primes.push_back(3);
+    primes.push_back(5);
+    primes.push_back(7);
+    primes.push_back(11);
+
+    std::cout << "There are: " << primes.size() << " elements in the vector." << std::endl;
+
+    std::cout << std::endl << std::endl;
+
+    std::cout << "The third element in the vector is: " << primes[2] << std::endl;
+
+    primes[2] = 17;
+    std::cout << "The third element in the vector is: " << primes[2] << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
