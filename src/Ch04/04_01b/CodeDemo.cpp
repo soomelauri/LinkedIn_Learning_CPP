6d6cfaf255ad1b05a9b0a861f07b2972863f39ec
soomelauri <143125206+soomelauri@users.noreply.github.com>
2024-03-17 20:46:04 +0000

    C

--- src/Ch04/04_01b/CodeDemo.cpp
+++ src/Ch04/04_01b/CodeDemo.cpp
@@ -5,7 +5,28 @@
 #include <iostream>
 
 int main(){
-    
+    int a = 1025;
+    bool flag = false;
+    char letter = 'f';
+
+    if (a > 1000)
+        std::cout << "Warning! a is greater than 1000! " << std::endl;
+
+    if (a % 2)
+        std::cout << "a is odd" << std::endl;
+    else
+        std::cout << "a is even" << std::endl;
+
+    std::cout << "The letter is ";
+    if (letter != 'a' && letter != 'e' && letter != 'o' && letter != 'u')
+        std::cout << "not ";
+    std::cout << " a vowel." << std::endl;
+
+    if (flag)
+        std::cout << "The flag is true." << std::endl;
+    else
+        std::cout << "The flag is false." << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
