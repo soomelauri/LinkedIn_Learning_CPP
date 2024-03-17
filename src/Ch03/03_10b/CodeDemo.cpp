ffcb237a4a9fc53a68816045f9f011256f7f46d0
soomelauri <143125206+soomelauri@users.noreply.github.com>
2024-03-17 15:22:07 +0000

    C++

--- src/Ch03/03_10b/CodeDemo.cpp
+++ src/Ch03/03_10b/CodeDemo.cpp
@@ -8,7 +8,19 @@
 #include "cow.h"
 
 int main(){
-    
+    std::vector <cow> cattle;
+
+    cattle.push_back(cow("Jimbo", 2, cow_purpose::meat));
+    cattle.push_back(cow("Betsy", 4, cow_purpose::hide));
+    cattle.push_back(cow("Tom", 9, cow_purpose::pet));
+    cattle.push_back(cow("Colleen", 5, cow_purpose::dairy));
+
+    std::cout << "The first cow is named: " << cattle.begin() ->get_name() << std::endl;
+    std::cout << "The second cow is named: " << cattle[1].get_name() << std::endl;
+    std::cout << "The second to last cow in named: " << prev(cattle.end(), 2) -> get_name() << std::endl;
+
+    std::cout << "The last cow is named: " << (cattle.end() - 1) -> get_name() << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
